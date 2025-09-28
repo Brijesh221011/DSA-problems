1b567de40f63aba3c9ba0f0f615f9ac5e8b2cc9c
Brijesh Yadav <136922197+Brijesh221011@users.noreply.github.com>
2025-09-28 12:54:38 +0530

    leetcode problem 976

---
+++ 976.Largest-Perimetre-Triangle/Solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Solution{
+    public static int largestPerimeter(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = nums.length - 1; i >= 2; i--) {
+            if (nums[i] < nums[i - 1] + nums[i - 2]) {
+                return nums[i] + nums[i - 1] + nums[i - 2];
+            }
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+       
+        int[] nums = {2,1,2};
+        int result = largestPerimeter(nums);
+        System.out.println("Largest Perimeter: " + result);
+    }
+}
