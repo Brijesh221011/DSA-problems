fe64a6fbb9086b7e0db5723876e823474701ee6c
Brijesh Yadav <136922197+Brijesh221011@users.noreply.github.com>
2025-09-30 12:54:58 +0530

    leetcode problem number 7

---
+++ 7.ReverseInteger/Solution.java
@@ -0,0 +1,19 @@
+
+
+public class Solution {
+    public static int reverse(int x) {
+        long res = 0;
+        while (x != 0) {
+            res = res * 10 + x % 10;
+            x /= 10;
+        }
+        return (res < Integer.MIN_VALUE || res > Integer.MAX_VALUE) ? 0 : (int) res;
+    }
+
+    public static void main(String[] args) {
+       // Solution solution = new Solution();
+        int x = 120;
+        int result = reverse(x);
+        System.out.println(result); // Output: -321
+    }
+}
