f2cb832bc52909f769f9c9fd1b38648ea0672b0c
Brijesh Yadav <136922197+Brijesh221011@users.noreply.github.com>
2025-10-04 09:44:04 +0530

    leetcode 35

---
+++ 35.SearchInsertPosition/Solution.java
@@ -0,0 +1,27 @@
+
+
+public class Solution {
+    public static int searchInsert(int[] nums, int target) {
+        int n=nums.length;
+        int start=0,end=nums.length-1;
+
+        while(start<=end){
+         int mid=(start+end)/2;
+
+         if(nums[mid]==target){
+           return mid;
+         }else if(nums[mid]<target){
+            start=mid+1;
+         }else{
+            end=mid-1;
+         }
+        }
+        return start;
+    }
+    public static void main(String[] args) {
+       
+        int[] nums = {1, 3, 5, 6};
+        int target = 5;
+        System.out.println(searchInsert(nums, target)); // Output: 2
+    }
+}
