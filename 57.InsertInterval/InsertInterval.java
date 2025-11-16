fdf40b58b9dcd1498ab830b1559d572904206231
Brijesh Yadav <136922197+Brijesh221011@users.noreply.github.com>
2025-11-16 12:28:07 +0530

    leetcode 57

---
+++ 57.InsertInterval/InsertInterval.java
@@ -0,0 +1,31 @@
+//leetcode 57: Insert Interval
+//time complexity: O(n)
+//space complexity: O(n)
+
+import java.util.ArrayList;
+import java.util.List;
+public class InsertInterval {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        int n=intervals.length;
+        List<int[]>res=new ArrayList<>();
+        int i=0;
+
+        while(i<n && intervals[i][1]<newInterval[0]){
+                res.add(intervals[i]);
+                i++;
+        }
+
+         while(i<n && intervals[i][0]<=newInterval[1]){
+            newInterval[0]=Math.min(intervals[i][0],newInterval[0]);
+            newInterval[1]=Math.max(intervals[i][1],newInterval[1]);
+            i++;
+         } 
+
+        res.add(newInterval);
+
+        while(i<n){
+            res.add(intervals[i++]);
+        }
+        return res.toArray(new int[res.size()][]);
+    }
+}
