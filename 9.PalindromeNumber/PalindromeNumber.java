8e8c065af8fe556d709ba4b7c1f8b96a0fe48235
Brijesh Yadav <136922197+Brijesh221011@users.noreply.github.com>
2025-11-21 21:04:28 +0530

    leetcode 9

---
+++ 9.PalindromeNumber/PalindromeNumber.java
@@ -0,0 +1,13 @@
+public class PalindromeNumber {
+     public boolean isPalindrome(int x) {
+        String str=Integer.toString(x);
+        int n=str.length();
+
+        for(int i=0;i<n/2;i++){
+            if(str.charAt(i)!=str.charAt(n-i-1)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
