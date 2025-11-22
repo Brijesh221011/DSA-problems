0f2f435e8447232d72fd68b5daa865e99c628604
Brijesh Yadav <136922197+Brijesh221011@users.noreply.github.com>
2025-11-22 11:22:00 +0530

    leetcode 3190

---
+++ 3190.FindMinOpToDivisibleBy3/MinOperations.java
@@ -0,0 +1,15 @@
+
+public class MinOperations {
+     public int minimumOperations(int[] nums) {
+        int n=nums.length;
+        int cnt=0;
+
+        for(int i=0;i<n;i++){
+            if(nums[i]%3!=0){
+               cnt++;
+            }
+            
+        }
+        return cnt;
+    }
+}
