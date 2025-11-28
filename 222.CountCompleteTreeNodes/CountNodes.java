b28c697fa686a4fc5c210269e6772aeeec98310c
Brijesh Yadav <136922197+Brijesh221011@users.noreply.github.com>
2025-11-28 11:50:17 +0530

    leetcode 222

---
+++ 222.CountCompleteTreeNodes/CountNodes.java
@@ -0,0 +1,27 @@
+
+public class CountNodes {
+    
+//   public class TreeNode {
+//       int val;
+//       TreeNode left;
+//       TreeNode right;
+//       TreeNode() {}
+//       TreeNode(int val) { this.val = val; }
+//       TreeNode(int val, TreeNode left, TreeNode right) {
+//           this.val = val;
+//           this.left = left;
+//           this.right = right;
+//       }
+//   }
+
+       public int countNodes(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        
+        int leftCnt=countNodes(root.left);
+        int rightCnt=countNodes(root.right);
+
+         return leftCnt+rightCnt+1;
+    }
+}
