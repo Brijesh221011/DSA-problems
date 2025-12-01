2b08a7c27caebd051bd7e0fad878cbd06b9729ed
Brijesh Yadav <136922197+Brijesh221011@users.noreply.github.com>
2025-12-01 11:26:50 +0530

    leetcode 152

---
+++ 152.MaximumProductSubarray/MaximumProd.java
@@ -0,0 +1,17 @@
+
+public class MaximumProd {
+    public int maxProduct(int[] nums) {
+        int maxProd=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            int prod=1;
+            for(int j=i;j<nums.length;j++){
+            prod*=nums[j];
+             
+            if(prod>maxProd){
+                maxProd=prod;
+            }
+            }
+        }    
+        return maxProd;
+    }
+}
