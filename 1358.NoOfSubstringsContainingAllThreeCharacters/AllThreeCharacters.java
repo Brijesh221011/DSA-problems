08d237083aff740acd601e5e4a350c07d575a0df
Brijesh Yadav <136922197+Brijesh221011@users.noreply.github.com>
2026-01-03 10:59:55 +0530

    leetcode 1358

---
+++ 1358.NoOfSubstringsContainingAllThreeCharacters/AllThreeCharacters.java
@@ -0,0 +1,18 @@
+
+public class AllThreeCharacters {
+     public int numberOfSubstrings(String s) {
+        int n=s.length();
+        int lastSeen[]={-1,-1,-1};
+        int cnt=0;
+
+        for(int i=0;i<n;i++){
+            char ch=s.charAt(i);
+            lastSeen[ch-'a']=i;
+
+            if(lastSeen[0]!=-1 && lastSeen[1]!=-1 && lastSeen[2]!=-1){
+                cnt=cnt+(1+Math.min(lastSeen[0],Math.min(lastSeen[1],lastSeen[2])));
+            }
+        }
+        return cnt;
+    }
+}
