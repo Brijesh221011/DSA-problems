3f035699d83d12b08f7317a98eedad150f51accb
Brijesh Yadav <136922197+Brijesh221011@users.noreply.github.com>
2026-01-29 20:53:02 +0530

    leetcode 42

---
+++ 42.TrappingRainWater/TrapingRainWater.java
@@ -0,0 +1,28 @@
+//Problem 42. Trapping Rain Water
+//time complexity: O(n),where n is the length of height array
+//space complexity: O(n),for leftMax and rightMax arrays
+
+public class TrapingRainWater {
+     public int trap(int[] height) {
+        int n=height.length; 
+        int leftMax[]=new int[n];
+            leftMax[0]=height[0];
+        for(int i=1;i<n;i++){
+           leftMax[i]=Math.max(leftMax[i-1],height[i]);
+        }
+        int rightMax[]=new int[n];
+        rightMax[n-1]=height[n-1];
+
+        for(int i=n-2;i>=0;i--){
+            rightMax[i]=Math.max(rightMax[i+1],height[i]);
+        }
+
+        int totWater=0;
+
+        for(int i=0;i<n;i++){
+            int curr=Math.min(leftMax[i],rightMax[i]);
+            totWater+=(curr-height[i]);
+        }
+        return totWater;
+    }
+}
