334ed750fd9230c0f947ee46dcc68fb4b7b2669c
Brijesh Yadav <136922197+Brijesh221011@users.noreply.github.com>
2026-01-31 11:47:45 +0530

    leetcode 744

---
+++ 744.FindSmallestLetterGreaterThanTarget/SmallestGtrThanTarget.java
@@ -0,0 +1,13 @@
+
+public class SmallestGtrThanTarget {
+     public char nextGreatestLetter(char[] letters, char target) {
+        
+        for(int i=0;i<letters.length;i++){
+            char ch=letters[i];
+            if(ch>target){
+                return ch;
+            }
+        }
+        return letters[0];
+    }
+}
