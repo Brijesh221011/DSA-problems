8b2b2d3b02ef4dd91e2feea800b2bafc6b4a2955
Brijesh Yadav <136922197+Brijesh221011@users.noreply.github.com>
2026-03-25 10:28:41 +0530

    leetcode 15

---
+++ 15.3Sum/3Sum.java
@@ -0,0 +1,41 @@
+class 3Sum{
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        int n=nums.length;
+        List<List<Integer>>res=new ArrayList<>();
+
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i-1]==nums[i]){
+                continue;
+            }
+            int start=i+1;
+            int end=n-1;
+            while(start<end){
+                int sum=nums[i]+nums[start]+nums[end];
+
+                if(sum==0){
+                    List<Integer>temp=new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[start]);
+                    temp.add(nums[end]);
+                    res.add(temp);
+
+                    start++;
+                    end--;
+
+                    while(start<end && nums[start]==nums[start-1]){
+                        start++;
+                    }
+                    while(start<end && nums[end]==nums[end+1]){
+                        end--;
+                    }
+                }else if(sum<0){
+                    start++;
+                }else{
+                    end--;
+                }
+            }
+        }
+        return res;
+    }
+}
