79ba20c4442e4cf8f6eb4848664b3fff8f165187
Brijesh Yadav <136922197+Brijesh221011@users.noreply.github.com>
2026-04-10 16:29:46 +0530

    leetcode 3740

---
+++ 3740.MinDistanceBetweenThreeEqualElements-I/MinDistanceI.java
@@ -0,0 +1,22 @@
+class MinDistanceI{
+   
+   public int minimumDistance(int[] nums) {
+        int n=nums.length;
+        int minDist=Integer.MAX_VALUE;
+
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                for(int k=j+1;k<n;k++){
+                    if(nums[i]==nums[j] && nums[j]==nums[k] && nums[k]==nums[i]){
+                        int dist=Math.abs(i-j)+Math.abs(j-k)+Math.abs(k-i);
+                        minDist=Math.min(minDist,dist);
+                    }
+                }
+            }
+        }
+        if(minDist==Integer.MAX_VALUE){
+            return -1;
+        }
+        return minDist;
+    }
+}
