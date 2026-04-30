81e834898acebe41a629244a1ed97f738fb5bf68
Brijesh Yadav <136922197+Brijesh221011@users.noreply.github.com>
2026-04-30 16:32:53 +0530

    leetcode 263

---
+++ 263.UglyNumber/UglyNumber.java
@@ -0,0 +1,26 @@
+
+//263. Ugly Number
+//time complexity: O(log n),where n is the input number
+//space complexity: O(1)
+
+class UglyNumber{
+     public boolean isUgly(int n) {
+        if (n <= 0) {
+            return false; 
+        }
+        
+        while (n % 2 == 0) {
+            n /= 2;
+        }
+        
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+        
+        while (n % 5 == 0) {
+            n /= 5;
+        }
+        
+        return n == 1;
+    }
+}
